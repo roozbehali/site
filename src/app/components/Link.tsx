b29de27ddbbd34acc7aa5f0fc2cd36743c921c0a
Roozbeh <roozbeh.ali.2005@gmail.com>
2024-06-07 04:06:52 -0400

    type fix

--- src/app/components/Link.tsx
+++ src/app/components/Link.tsx
@@ -1,4 +1,11 @@
-const Link = ({ href, children }) => {
+import React, { ReactNode } from "react";
+
+interface LinkProps {
+  href: string;
+  children: ReactNode;
+}
+
+const Link: React.FC<LinkProps> = ({ href, children }) => {
   return (
     <a href={href} target="_blank" className="text-zinc-500 underline decoration-1 underline-offset-2 decoration-dashed hover:text-orange-200 transition duration-300">
       {children}
